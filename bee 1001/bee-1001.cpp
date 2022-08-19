f62fa8e334950b9382b481b9267824f8dd659231
Jaime Oliveira <hello@jaimeoliveira.io>
2022-08-19 00:01:44 -0300

    Solved problem 1001 in C++

---
+++ bee 1001/bee-1001.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int firstNumber, secondNumber;
+    
+    cin >> firstNumber;
+    cin >> secondNumber;
+
+    cout << "X = " << firstNumber + secondNumber << endl;
+
+    return 0;
+}
