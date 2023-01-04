9371bd77fcbfc03e430dba57ceb954ce06e09193
Jaime  Oliveira <hello@jaimeoliveira.io>
2023-01-03 22:01:17 -0300

    Solved problem 1008 in Python

---
+++ bee 1008/bee-1008.py
@@ -0,0 +1,7 @@
+numero_funcionario, horas_trabalhadas = int(input()), int(input())
+valor_hora = float(input())
+
+salario = horas_trabalhadas * valor_hora
+
+print("NUMBER =", numero_funcionario)
+print("SALARY = U$ %.2f" %salario)
